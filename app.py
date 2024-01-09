368959a08aac8b7ebdf5680897aa680e73d06fec
lohith <lola@lohith.local>
2024-01-09 19:22:54 +0100

    chnage

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from integral import calculate_integrals
 from flask import Flask, jsonify
+from integral import calculate_integrals
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to the Integral Calculator! Use the /integral/<lower>/<upper> endpoint to calculate integrals.'
 
 @app.route('/integral/<lower>/<upper>', methods=['GET'])
 def integral_service(lower, upper):
     result = calculate_integrals(float(lower), float(upper))
     return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
